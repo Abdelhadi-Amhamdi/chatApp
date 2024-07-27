4dc491db26d1eed9f182c7ceee891dbaa7ef88ae
Abdelhadi Amhamdi <aamhamdi@e3r7p12.1337.ma>
2024-07-27 14:59:16 +0100

    [add login component]

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import {createBrowserRouter, RouterProvider, Link} from 'react-router-dom'
 import './index.css'
 import webSocketService from './socket'
 import Index from './routes'
@@ -52,6 +52,40 @@ function Room() {
 	)
 }
 
+function Login() {
+	return (
+		<>
+			<div className='h-full flex justify-center items-center'>
+				<div className='p-10 w-[50%] h-[32vh] rounded-lg border-[1px]'>
+					<div className="header block text-center">
+						<h1 className='text-2xl'>Welcome Back</h1>
+					</div>
+					<div className='mt-10'>
+						<label className='block w-full' htmlFor="username">username :</label>
+						<input 
+							className='block bg-[#f7f7f7] px-4 mt-2 border-[1px] rounded-lg h-[40px] w-full' 
+							type="text" 
+							name='username'
+							placeholder='joe'
+						/>
+						<label className='block mt-4' htmlFor="password">password : </label>
+						<input 
+							className='block mt-2 bg-[#f7f7f7] border-[1px] px-4 rounded-lg h-[40px] w-full'
+							type="password" 
+							name='password'
+							placeholder='*********'
+						/>
+						<p className='mt-4 text-[12px]'>forget Password ?</p>
+						<button className='mt-4 block bg-[#f7f7f7] w-full h-[40px] rounded-lg border-[1px] uppercase'>login</button>
+						<p className='mt-4 text-[12px]'>you dont have account ? <Link to="/auth/signup">signup</Link></p>
+		
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
+
 const router = createBrowserRouter([
 	{
 		path : '/',
@@ -60,6 +94,14 @@ const router = createBrowserRouter([
 			{
 				path : '/room/:roomId',
 				element : <Room />,
+			},
+			{
+				path : '/auth/login',
+				element : <Login />
+			},
+			{
+				path : '/auth/signup',
+				element : <div>signup</div>
 			},
 		]
 	},
--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -10,13 +10,27 @@ export default function Index() {
 	return (
 		<>
 			<div className='flex h-[100vh] border-[#e3e3e3]'>
-				<div className='w-[25vw] bg-[#f7f7f7] border-r-[1px]'>
+				<div className='w-[25vw] bg-[#f7f7f7] border-r-[1px] relative'>
 					<div className='h-[80px] border-b-[1px] flex justify-center py-4'>
 						
 						<input type="text" placeholder='join room ...' className='border-[1px] w-[16vw] rounded-lg p-2' />
 						<Link to="/room/abc">
-							<button className='bg-white h-full ml-2 border-[1px] flex justify-center items-center rounded-lg px-2 py-1 w-[60px]'>new</button>
+							<button disabled={true} className='bg-white h-full ml-2 border-[1px] flex justify-center items-center rounded-lg px-2 py-1 w-[60px]'>new</button>
 						</Link>
+					</div>
+					<div className="h-[60px] w-full absolute bottom-0 p-2 border-t-[1px] flex items-center">
+						<ul className="flex w-full px-2">
+							<li className="bg-white p-2 rounded-lg border-[1px] mr-2 cursor-pointer">
+								<Link to="/auth/login">
+									login
+								</Link>
+							</li>
+							<li className="bg-white p-2 rounded-lg border-[1px]">
+								<Link to="/auth/signup">
+									signup
+								</Link>
+							</li>
+						</ul>
 					</div>
 				</div>
 				<div className='w-[75vw] p-2'>
